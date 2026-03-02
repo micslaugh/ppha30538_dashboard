5280bec1e9c653cad689b36b8e7b4e6bc569b199
Micah Slaughter <micahs@uchicago.edu>
2026-03-02 01:24:15 -0600

    code test

--- dash_code.py
+++ dash_code.py
@@ -9,7 +9,7 @@ DASHBOARD_DIR = "data/derived-data"
 HEALTH_FILE = {f"{DASHBOARD_DIR}/outcomes.csv"}
 AQI_FILE = {f"{DASHBOARD_DIR}/aqi.csv"}
 
-outcomes = pd.read_csv("outcomes.csv")
+outcomes = pd.read_csv("data/derived-data/outcomes.csv")
 
 health_long = outcomes.melt(
     id_vars="County",
@@ -32,13 +32,34 @@ st.title("# Defining Parameters of Carbon Emissions and their Effects on Communi
 st.write("How do varying carbon emissions affect the AQI of Illinois and what effects do they have on health? We generated graphs tracking various factors, measured by county.")
 
 ##Load Data
+@st.cache_data
+def load_outcomes():
+    outcomes = pd.read_csv("data/derived-data/outcomes.csv")
+    return outcomes
 
+@st.cache_data
+def load_aqi():
+    aqi = pd.read_csv("data/derived-data/aqi.csv")
+    return aqi
+
+outcomes_df = load_outcomes()
+aqi_df = load_aqi()
+
+health_outcomes = [col for col in outcomes_df.columns if col not in ["County", "Total Direct Emissions", "Population"]]
 
 
 ##User Inputs
 
 param = st.selectbox("Select Health Outcome", ["Asthma Incidence", "COPD Deaths", "COVID Deaths", "Heart Failures", "Stroke Deaths"])
 st.subheader(f"Selected View: {param}")
+
+color_palette = [
+    "#cf1020", # 
+    "#A4343A", # 
+    "#800000", # 
+    "#550000", # 
+    "#2c1608"  # 
+]
 
 carb_chart = alt.Chart(health_long).mark_line(point=True).encode(
     x=alt.X("County:N", title="Counties", sort=None),
